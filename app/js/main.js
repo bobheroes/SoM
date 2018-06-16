9a9eaf63bc1ccace944d63a89492b5c78b2cbbae
El <bobtheheroes@gmail.com>
2018-06-16 22:00:24 +0300

    new news

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,30 @@ $(document).ready(function(){
         autoplay:true,
         autoplaySpeed: 2000,
         autoplayTimeout: 10000,
-    });  
-  });
+        autoHeight: true,
+    }); 
+    
+    $(window).resize(function() {
+        $("nav li").attr('style', "");
+      });
 
-  $("nav li:nth-child(3)").click(function(){
-    if($(window).width() <= '768'){
+      
+$( ".plot button" ).click(function(){
+    $(".plot p:gt(2)").slideToggle();
+    ($(this).text() == "Подробнее") ? $(this).text("Скрыть") : $(this).text("Подробнее") ;
+})
+
+$(".fake-label input[type='checkbox']").click(function(){
+    $('button[type="submit"]').prop('disabled', function(i, v) { return !v; });
+})
+
+$("nav li:nth-child(3)").click(function(){
+    if($(window).width() <= '767'){
            $(this).closest('ul').find('li').not(this).slideToggle();
     }
 })
+});
+
+
+
+
